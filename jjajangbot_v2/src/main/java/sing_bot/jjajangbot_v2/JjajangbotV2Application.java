9bf22bad664dfe057301aff925604c9dcdbedc16
gimeng11 <2071199@hansung.ac.kr>
2025-08-24 13:38:55 +0900

    Initial commit

---
+++ jjajangbot_v2/src/main/java/sing_bot/jjajangbot_v2/JjajangbotV2Application.java
@@ -0,0 +1,13 @@
+package sing_bot.jjajangbot_v2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JjajangbotV2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JjajangbotV2Application.class, args);
+	}
+
+}
---
+++ jjajangbot_v2/src/test/java/sing_bot/jjajangbot_v2/JjajangbotV2ApplicationTests.java
@@ -0,0 +1,13 @@
+package sing_bot.jjajangbot_v2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JjajangbotV2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
